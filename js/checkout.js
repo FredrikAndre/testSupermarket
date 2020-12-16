64958e8a22e6c4cca1826ac035b419658c506d7c
FredrikAndre <fredrik.he.andre@gmail.com>
2020-12-16 11:07:27 +0100

    Did some refaktorering

--- js/checkout.js
+++ js/checkout.js
@@ -22,48 +22,31 @@ $(function() {
     $("<button>").addClass("btn btn-success").attr('id', "checkoutdone").text("Betala").appendTo(".paybtn");
     $("#checkoutdone").on('click', function() {
         window.location.assign('../html/thankyou.html');
-    });
+    }); 
 
 });
 
-function generateCheckout(){
+function generateCheckout() {
 
 let totalValue = 0;
 
 $.each(cart, (i, product)=>{
 
+    let myTR =  $("<tr>")
+    .appendTo(".cartcontainer");
 
+    $("<td>").text(product.title).appendTo(myTR);
+    $("<td>").text(product.price + " kr").appendTo(myTR);
+    $("<td>").text(product.quantity).appendTo(myTR);
+    $("<td>").text(product.price * product.quantity + " kr").appendTo(myTR);
+    totalValue += product.price * product.quantity;
+    });
 
-let myTR =  $("<tr>")
-.appendTo(".cartContainer");
+    $("<tr>").attr("id", "cartTotalRow").appendTo(".cartcontainer");
+    $("<th>").text("Total").appendTo("#cartTotalRow");
+    $("<td>").text("-").appendTo("#cartTotalRow");
+    $("<td>").text("-").appendTo("#cartTotalRow");
+    $("<td>").text(totalValue + " kr").appendTo("#cartTotalRow");
 
-$("<td>")
-.text(product.title)
-.appendTo(myTR);
-$("<td>")
-.text(product.price + " " + "kr")
-.appendTo(myTR);
-$("<td>")
-.text(product.quantity)
-.appendTo(myTR);
-$("<td>")
-.text(product.price * product.quantity + " kr")
-.appendTo(myTR);
-totalValue += product.price * product.quantity;
-})
-$("<tr>")
-.attr("id", "cartTotalRow")
-.appendTo(".cartContainer");
-$("<th>")
-.text("Total")
-.appendTo("#cartTotalRow");
-$("<td>")
-.text("-")
-.appendTo("#cartTotalRow");
-$("<td>")
-.text("-")
-.appendTo("#cartTotalRow");
-$("<td>")
-.text(totalValue + " " + "kr")
-.appendTo("#cartTotalRow");
 }
+ 
