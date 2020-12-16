ef4bcaddfa55b6d857711c4fc7b496c4bef2b3ad
FredrikAndre <fredrik.he.andre@gmail.com>
2020-12-16 23:57:49 +0100

    Fixed SCSS

--- js/checkout.js
+++ js/checkout.js
@@ -13,10 +13,10 @@ $(function() {
     $(".navbar-brand").on('click', function() {
         window.location.assign('../index.html');
     });
-    // Button back to "product side"
+    // Button back to previous Page
     $("<button>").addClass("btn btn-danger mr-3").attr('id', "toproducts").text("Tillbaka").appendTo(".paybtn");
     $("#toproducts").on('click', function() {
-        window.location.assign('../html/products.html');
+        window.history.back();
     });
     // Button checkout to "Thank you side"
     $("<button>").addClass("btn btn-success").attr('id', "checkoutdone").text("Betala").appendTo(".paybtn");
@@ -30,7 +30,7 @@ function generateCheckout() {
 
 let totalValue = 0;
 
-$.each(cart, (i, product)=>{
+$.each(cart, (i, product)=> {
 
     let myTR =  $("<tr>")
     .appendTo(".cartcontainer");
@@ -40,13 +40,19 @@ $.each(cart, (i, product)=>{
     $("<td>").text(product.quantity).appendTo(myTR);
     $("<td>").text(product.price * product.quantity + " kr").appendTo(myTR);
     totalValue += product.price * product.quantity;
+    $("<td>").html('<i class="deletebtn fas fa-trash"></i>').appendTo(myTR)
+
+    $(".deletebtn").click(function() {
+        $(this).remove();
     });
 
+    });
+    
     $("<tr>").attr("id", "carttotalrow").appendTo(".cartcontainer");
-    $("<th>").text("Total").appendTo("#carttotalrow");
-    $("<td>").text("-").appendTo("#carttotalrow");
-    $("<td>").text("-").appendTo("#carttotalrow");
+    $("<th>").text("Total Summa").appendTo("#carttotalrow");
+    $("<td>").appendTo("#carttotalrow");
+    $("<td>").appendTo("#carttotalrow");
     $("<td>").text(totalValue + " kr").appendTo("#carttotalrow");
 
+
 }
- 
